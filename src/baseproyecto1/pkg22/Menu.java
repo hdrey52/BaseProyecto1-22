52c5d0aed09222ef01e49bca2dd5395c8b3132b2
Ramon Dominguez <Ramon Dominguez@DESKTOP-9U23PA3>
2022-04-09 21:40:33 -0400

    Arreglado el problema v2

--- src/baseproyecto1/pkg22/Menu.java
+++ src/baseproyecto1/pkg22/Menu.java
@@ -230,7 +230,7 @@ void limpiar(){
 
         MenuBar.add(jMenu3);
 
-        jMenu4.setText("Salire");
+        jMenu4.setText("Salir");
 
         jMenuItem11.setText("Cerrar Sesion");
         jMenuItem11.addActionListener(new java.awt.event.ActionListener() {
